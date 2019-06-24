504473cc6feceec4998d4fd702e1f8f865a367f3
axlin <axlin@linkedin.com>
2019-06-24 00:42:23 -0700

    cleanup bike.py a bit

--- bike.py
+++ bike.py
@@ -6,6 +6,7 @@ import random, json
 app = Flask(__name__)
 
 dict_list = []
+
 def loadData():
     filename = "201904-fordgobike-tripdata.csv"
     with open(filename) as data:
@@ -13,97 +14,67 @@ def loadData():
         for line in reader:
             dict_list.append(line)
 
+
 def station_to_coordinates():
     dictionary = dict()
     for entry in dict_list:
         if entry["start_station_name"] not in dictionary:
             dictionary[entry["start_station_name"]] = {"lat": float(entry["start_station_latitude"]), "lng": float(entry["start_station_longitude"])}
-    # print(dictionary)
     return dictionary
 
-# def getEndsForTarget(target):
-#     lat = 0
-#     lon = 0
-#     end_lats = []
-#     end_lons = []
-#     print(target)   
-#     for entry in dict_list:
-#         if entry["start_station_name"] == target:
-#             end_lats.append(float(entry["end_station_latitude"]))
-#             end_lons.append(float(entry["end_station_longitude"]))
-#             lat = float(entry["start_station_latitude"])
-#             lon = float(entry["start_station_longitude"])
-#     return lat, lon, end_lats, end_lons
-
-
-def getStartsForTarget(target):
+def getCoordinates(target):
     lat = 0
     lon = 0
-    end_lats = []
-    end_lons = []
-    print(target)   
+    target_lats = []
+    target_lons = []
     for entry in dict_list:
         if entry["end_station_name"] == target:
-            end_lats.append(float(entry["start_station_latitude"]))
-            end_lons.append(float(entry["start_station_longitude"]))
+            target_lats.append(float(entry["start_station_latitude"]))
+            target_lons.append(float(entry["start_station_longitude"]))
             lat = float(entry["end_station_latitude"])
             lon = float(entry["end_station_longitude"])
-    return lat, lon, end_lats, end_lons
-
-# def getStartsForTargetByCoords(lat, lon):
-#     end_lats = []
-#     end_lons = []
-#     for entry in dict_list:
-#         if entry["end_station_latitude"] == lat and entry["end_station_longitude"] == lon:
-#             end_lats.append(float(entry["start_station_latitude"]))
-#             end_lons.append(float(entry["start_station_longitude"]))
-#     return end_lats, end_lons
+    return lat, lon, target_lats, target_lons
 
 
-# @app.route('/findEndStations')
-# def findByStartStation():
-#     stationName = request.args.get('stationName')
-#     return make_map_start(stationName)
+@app.route('/')
+def index():
+    stationName = "5th St at Folsom"  # set a default
+    return make_map(stationName, 13)
+
 
 @app.route('/findStartStations')
 def findByEndStation():
     stationName = request.args.get('stationName')
     zoom = request.args.get('zoom')
-    if zoom == None:
+    if stationName not in station_to_coordinates():
+        stationName = "5th St at Folsom"  # set a default
+    if zoom is None:
         zoom = 13
-    # if stationName == None:
-    #     lat = request.args.get('lat')
-    #     lon = request.args.get('lon')
-    #     return make_map_end_coords(lat, lon)
-    return make_map_end(stationName, zoom)
+    return make_map(stationName, zoom)
 
 
-# def make_map_start(stationName):
-#     print(stationName)
-#     target_lat, target_lon, end_lats, end_lons = getEndsForTarget(stationName)
-#     print(target_lat, target_lon)
-#     stations = list(set([entry["start_station_name"] for entry in dict_list]))
-#     return render_template('index.html', lat=target_lat, lon=target_lon, end_lats=end_lats, end_lons=end_lons, stationName=stationName, station_to_coordinates=station_to_coordinates())
+@app.route('/pointsData')
+def pointsData():
+    stationName = request.args.get('stationName')
+    lat, lon, target_lats, target_lons = getCoordinates(stationName)
+    return json.dumps({
+        "lat": lat,
+        "lon": lon,
+        "target_lats": target_lats,
+        "target_lons": target_lons
+    })
 
-def make_map_end(stationName, zoom):
-    print(stationName)
-    target_lat, target_lon, end_lats, end_lons = getStartsForTarget(stationName)
-    print(target_lat, target_lon)
-    stations = list(set([entry["start_station_name"] for entry in dict_list]))
+
+def make_map(stationName, zoom):
+    target_lat, target_lon, end_lats, end_lons = getCoordinates(stationName)
     return render_template('index.html', lat=target_lat, lon=target_lon, end_lats=end_lats, end_lons=end_lons, stationName=stationName, station_to_coordinates=station_to_coordinates(), zoom=zoom)
-
-
-# def make_map_end_coords(lat, lon):
-#     end_lats, end_lons = getStartsForTargetByCoords(lat, lon)
-#     stations = list(set([entry["start_station_name"] for entry in dict_list]))
-#     return render_template('index.html', lat=lat, lon=lon, end_lats=end_lats, end_lons=end_lons, stationName=stationName, station_to_coordinates=station_to_coordinates())
 
 
 if __name__ == '__main__':
     loadData()
     app.run()
 
-# questions to answer 
+# questions to answer
 # when do people ride
 # where are pick ups / drop offs on the map
 # most common routes?
