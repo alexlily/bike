814acc72567090c79b395b3c9c26df578878869d
axlin <axlin@linkedin.com>
2019-06-25 00:46:12 -0700

    create index.html, rename the map to map.html, find most popular source and destination stops

--- bike.py
+++ bike.py
@@ -22,6 +22,25 @@ def station_to_coordinates():
             dictionary[entry["start_station_name"]] = {"lat": float(entry["start_station_latitude"]), "lng": float(entry["start_station_longitude"])}
     return dictionary
 
+
+def end_station_to_visits():
+    dictionary = dict()
+    for entry in dict_list:
+        if entry["end_station_name"] not in dictionary:
+            dictionary[entry["end_station_name"]] = 0
+        dictionary[entry["end_station_name"]] += 1
+
+    return [(key, dictionary[key]) for key in sorted(dictionary, key= lambda item: dictionary[item], reverse=True)]
+
+def start_station_to_visits():
+    dictionary = dict()
+    for entry in dict_list:
+        if entry["start_station_name"] not in dictionary:
+            dictionary[entry["start_station_name"]] = 0
+        dictionary[entry["start_station_name"]] += 1
+    return [(key, dictionary[key]) for key in sorted(dictionary, key= lambda item: dictionary[item], reverse=True)]
+
+
 def getCoordinates(target):
     lat = 0
     lon = 0
@@ -38,8 +57,7 @@ def getCoordinates(target):
 
 @app.route('/')
 def index():
-    stationName = "5th St at Folsom"  # set a default
-    return make_map(stationName, 13)
+    return render_template("index.html")
 
 
 @app.route('/findStartStations')
@@ -47,7 +65,7 @@ def findByEndStation():
     stationName = request.args.get('stationName')
     zoom = request.args.get('zoom')
     if stationName not in station_to_coordinates():
-        stationName = "5th St at Folsom"  # set a default
+        stationName = "San Francisco Caltrain Station 2  (Townsend St at 4th St)"  # set a default
     if zoom is None:
         zoom = 13
     return make_map(stationName, zoom)
@@ -58,6 +76,7 @@ def pointsData():
     stationName = request.args.get('stationName')
     lat, lon, target_lats, target_lons = getCoordinates(stationName)
     return json.dumps({
+        "stationName": stationName,
         "lat": lat,
         "lon": lon,
         "target_lats": target_lats,
@@ -65,9 +84,21 @@ def pointsData():
     })
 
 
+@app.route('/popularDestination')
+def sortByMostPopularDestination():
+    return json.dumps(end_station_to_visits())
+
+
+@app.route('/popularSource')
+def sortByMostPopularSource():
+    return json.dumps(start_station_to_visits())
+
+
 def make_map(stationName, zoom):
     target_lat, target_lon, end_lats, end_lons = getCoordinates(stationName)
-    return render_template('index.html', lat=target_lat, lon=target_lon, end_lats=end_lats, end_lons=end_lons, stationName=stationName, station_to_coordinates=station_to_coordinates(), zoom=zoom)
+    return render_template('map.html', lat=target_lat, lon=target_lon, end_lats=end_lats,
+        end_lons=end_lons, stationName=stationName, station_to_coordinates=station_to_coordinates(), 
+        zoom=zoom)
 
 
 if __name__ == '__main__':
